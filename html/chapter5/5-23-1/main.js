6a94b98930b9420e69b91815c7ed5a9c7291c215
91274750 <ppabew@icloud.com>
2022-12-17 03:24:22 +0900

    add: 중요 파일 추가 modify: 5-23, 5-24 소스 파일 수정

--- html/chapter5/5-23-1/main.js
+++ html/chapter5/5-23-1/main.js
@@ -3,8 +3,37 @@ Vue.component('my-component', {
 })
 
 Vue.component('comp-child', {
-  template: '<p>{{val}}</p>',
+  template: '<p id="child" class="child">{{val}}</p>',
   props: ['val']
+})
+
+var bus = new Vue({
+  data: {
+    count: 0
+  }
+})
+
+Vue.component('component-a', {
+  template: '<p><button v-on:click="clickButton">클릭</button></p>',
+  methods: {
+    clickButton: function () {
+      bus.$emit('bus-event')
+    }
+  }
+})
+
+Vue.component('component-b',{
+  template: '<p>bus: {{bus.count}}</p>',
+  computed: {
+    bus: function () {
+      return bus.$data
+    }
+  },
+  created: function () {
+    bus.$on('bus-event', function () {
+      this.count++
+    })
+  }
 })
 
 // eslint-disable-next-line no-undef
--- html/chapter5/5-24-1/main.js
+++ html/chapter5/5-24-1/main.js
@@ -1,5 +1,11 @@
 Vue.component('comp-child', {
-  // 템플릿에서 val 사용하기
-  template: '<p></p>',
-  // 받을 속성 이름 지정하기
+  template: '<p>dd</p>',
+})
+
+
+// eslint-disable-next-line no-undef
+var app = new Vue({
+  el: '#app',
+  data: {
+  }
 })
